4863389871f5d1fa1ca2cfe939352aa648f28212
nawal abdualghani <nawalabdualghani@gmail.com>
2019-11-13 08:52:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ const birthday = [
   "Thank you, Brendan, for the wonderful birthday gift!",
   "Thank you, Ali, for the wonderful birthday gift!"
 ];
+function wrapGifts(name,birthday) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+ 
+  return gifts;
+}
+ 
+wrapGifts(gifts);
