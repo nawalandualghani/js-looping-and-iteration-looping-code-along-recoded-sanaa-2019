86c06d93f652f2029636a4ae1adf5478a35b80fc
nawal abdualghani <nawalabdualghani@gmail.com>
2019-11-13 08:51:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 // Code your solutions in this file
 
 const name = ["Ada", "Brendan", "Ali"];
+const birthday = [
+  "Thank you, Ada, for the wonderful birthday gift!",
+  "Thank you, Brendan, for the wonderful birthday gift!",
+  "Thank you, Ali, for the wonderful birthday gift!"
+];
