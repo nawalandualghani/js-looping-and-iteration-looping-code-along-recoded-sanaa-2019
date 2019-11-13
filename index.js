5a79189e69fe80ee98881e007c1d2f7944f5cddd
nawal abdualghani <nawalabdualghani@gmail.com>
2019-11-13 08:54:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function wrapGifts(name,birthday) {
   for (let i = 0; i < name.length; i++)
   for (let j=0; j<birthday.length;j++ ){
     console.log(`Wrapped ${name[i]} and added a bow!`);
+    console.console.log(`birthday ${birthday[i]}`);
   }
 
   return gifts;
